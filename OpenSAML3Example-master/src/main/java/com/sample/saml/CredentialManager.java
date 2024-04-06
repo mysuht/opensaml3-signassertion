8b06c8ccb07b964f7d8af339040608414ecbaafa
mysuht <mysuht@gmail.com>
2024-04-07 00:33:42 +0300

    committed

---
+++ OpenSAML3Example-master/src/main/java/com/sample/saml/CredentialManager.java
@@ -0,0 +1,106 @@
+package com.sample.saml;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+
+import org.opensaml.security.x509.BasicX509Credential;
+import org.opensaml.security.x509.X509Credential;
+
+public class CredentialManager {
+
+    private static final String CRT_FILE_PATH = "./credentials/mycertificate.crt";
+    private static final String PK_FILE_PATH = "./credentials/pcks8key.der";
+
+
+    public static X509Credential loadCredential() {
+        try {
+            // Step 1: Read the Certificate File (.crt)
+            X509Certificate cert = loadCertificate();
+
+            // Step 2: Read the Private Key File (.der)
+            PrivateKey privateKey = loadPrivatekey();
+
+            // Step 3: Create X509Credential
+            BasicX509Credential credential = new BasicX509Credential(cert, privateKey);
+
+            return credential;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static X509Certificate loadCertificate() throws FileNotFoundException, CertificateException {
+        FileInputStream crtFis = new FileInputStream(CRT_FILE_PATH);
+        CertificateFactory cf = CertificateFactory.getInstance("X.509");
+        X509Certificate cert = (X509Certificate) cf.generateCertificate(crtFis);
+        return cert;
+    }
+
+    private static PrivateKey loadPrivatekey() throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+        FileInputStream keyFis = new FileInputStream(PK_FILE_PATH);
+        byte[] keyBytes = new byte[keyFis.available()];
+        keyFis.read(keyBytes);
+        keyFis.close();
+
+        // Remove any occurrences of &#13; from the keyBytes
+        String privateKeyString = new String(keyBytes, "UTF-8").replace("&#13;", "");
+        byte[] cleanedKeyBytes = privateKeyString.getBytes("UTF-8");
+
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyBytes);
+        KeyFactory kf = KeyFactory.getInstance("RSA"); // Assuming RSA key, adjust if needed
+        PrivateKey privateKey = kf.generatePrivate(keySpec);
+        return privateKey;
+    }
+
+    public static X509Credential getCredential() {
+        try {
+            // Step 1: Read the Certificate File
+            X509Certificate cert = getX509Certificate();
+
+            // Step 3: Create X509Credential
+            BasicX509Credential credential = new BasicX509Credential(cert);
+
+            // Optionally, you can set additional properties for the credential
+            // credential.setPrivateKey(privateKey); // If you have the private key
+
+            return credential;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static X509Certificate getX509Certificate() throws FileNotFoundException, CertificateException {
+        X509Certificate cert = loadCertificate();
+        return cert;
+    }
+
+    public static X509Certificate loadPublicKey(X509Certificate crt) throws IOException, CertificateException {
+        return crt;
+    }
+
+    // Example usage
+    public static void main(String[] args) {
+        X509Credential credential = loadCredential();
+
+        if (credential != null) {
+            System.out.println("Credential obtained successfully!");
+        } else {
+            System.out.println("Failed to obtain credential.");
+        }
+    }
+}
+
---
+++ OpenSAML3Example-master/src/test/java/sample/SAMLResponseBuilderTest.java
@@ -0,0 +1,66 @@
+package sample;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.time.Instant;
+
+import com.sample.saml.CredentialManager;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.opensaml.saml.saml2.core.Assertion;
+import org.opensaml.saml.saml2.core.EncryptedAssertion;
+import org.opensaml.saml.saml2.core.Response;
+import org.opensaml.security.SecurityException;
+import org.opensaml.security.x509.BasicX509Credential;
+
+import com.sample.saml.KeyUtil;
+import com.sample.saml.SAMLResponseBuilder;
+import com.sample.saml.SAMLUtil;
+import org.opensaml.security.x509.X509Credential;
+
+class SAMLResponseBuilderTest {
+	private static BasicX509Credential privateKeyCredential;
+	private static BasicX509Credential publicKeyCredential;
+	
+	@BeforeAll
+	private static void setUp() throws Exception {
+//		System.setProperty("org.apache.xml.security.ignoreLineBreaks", "true");
+		String crtFilePath = "./credentials/mycertificate.crt";
+		X509Certificate basicCredential = CredentialManager.getX509Certificate();
+		X509Certificate crt = CredentialManager.loadPublicKey(basicCredential);
+
+		X509Certificate publicKey = CredentialManager.loadPublicKey(crt); //KeyUtil.loadPublicKey(new File("./keystore/public_key.cer"));
+		publicKeyCredential = new BasicX509Credential(publicKey);
+		privateKeyCredential = (BasicX509Credential) KeyUtil.getCredential();
+	}
+
+	@Test
+	void validateSAMLResponse() throws FileNotFoundException, CertificateException, SecurityException {
+		String encodedEncryptedSignedSAMLXMLString = generateEncodedEncryptedSignedSAMLXMLString();
+
+		System.out.println("encodedEncryptedSignedSAMLXMLString >> " + encodedEncryptedSignedSAMLXMLString);
+
+		String decodeString = SAMLUtil.base64Decode(encodedEncryptedSignedSAMLXMLString);
+		System.out.println("decodeString >> " + decodeString);
+//		EncryptedAssertion encryptedAssertion = SAMLUtil.getSamlEncryptedAssertion(decodeString);
+//		Assertion assertion = SAMLUtil.decryptAssertion(encryptedAssertion, CredentialManager.loadCredential());
+//		Assertions.assertTrue(SAMLUtil.isValidAssertionSignature(assertion, CredentialManager.loadCredential()), "Signature must be valid");
+//		System.out.println(SAMLUtil.stringifySAMLObject(assertion));
+	}
+	
+	public static String generateEncodedEncryptedSignedSAMLXMLString() throws FileNotFoundException, CertificateException, SecurityException {
+		SAMLResponseBuilder samlResponse = new SAMLResponseBuilder();
+		Response response = samlResponse.buildResponse();
+		response.setInResponseTo("abcd");
+		Assertion assertion = samlResponse.buildAssertion(response.getID(), response.getIssueInstant(), "idCome", "idTwo");
+		SAMLUtil.signAssertion(assertion, CredentialManager.loadCredential());
+//		EncryptedAssertion encryptedAssertion = SAMLUtil.encryptAssertion(assertion, publicKeyCredential);
+		// response.getEncryptedAssertions().add(encryptedAssertion);
+		 response.getAssertions().add(assertion);
+		String strResponse = SAMLUtil.stringifySAMLObject(response);
+		return SAMLUtil.base64Encode(strResponse);		
+	}
+}
